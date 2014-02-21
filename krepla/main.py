86c9bb8b0e339462ab5216da9af54e3f5a7c485c
Ben Rousch <brousch@gmail.com>
2014-02-21 17:01:32 -0500

    Working collapse/expand for output

--- krepla/main.py
+++ krepla/main.py
@@ -14,27 +14,50 @@ __version__ = '0.1'
 
 
 class HistoryInput(BoxLayout):
-    def collapse_row(self, lbl):
+    def collapse_row(self, app, lbl):
         if lbl.shorten:
             lbl.shorten = False
         else:
             lbl.shorten = True
-        app = App.get_running_app()
         Clock.schedule_once(app.root.recalc_height)
+
+
+class HistoryOutput(BoxLayout):
+    def collapse_row(self, app, lbl):
+        if lbl.shorten:
+            lbl.shorten = False
+        else:
+            lbl.shorten = True
+        Clock.schedule_once(app.root.recalc_height)
+
 
 class Krepla(BoxLayout):
     line_count = 0
     io_history = ObjectProperty(None)
 
     def run_input(self, command):
+        # Add input to history
         self.line_count += 1
         row = HistoryInput()
         row.line_num = str(self.line_count)
         row.input_text = command
         self.io_history.add_widget(row)
+
+        # Add output to history
+        self.line_count += 1
+        row = HistoryOutput()
+        row.line_num = str(self.line_count)
+        row.output_text = 'Output: ' + command
+        self.io_history.add_widget(row)
+
+        # Work-around for height issues
         Clock.schedule_once(self.recalc_height)
-    
+
     def recalc_height(self, dt):
+        ''' A method to add and remove a widget from the io_history to force
+            the recalculation of its height. Without this, the scrollview will
+            not work correctly.
+        '''
         work_around = Widget()
         self.io_history.add_widget(work_around)
         self.io_history.remove_widget(work_around)
