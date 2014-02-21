8ce12b80f2730fa4831b7c46ea7c4d8a687b745d
Ben Rousch <brousch@gmail.com>
2014-02-21 14:30:30 -0500

    Working scrollview and inputs

--- krepla/main.py
+++ krepla/main.py
@@ -4,14 +4,25 @@ import kivy
 kivy.require('1.8.0')
 
 from kivy.app import App
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 
 
 __version__ = '0.1'
 
 
-class Krepla(BoxLayout):
+class HistoryInput(BoxLayout):
     pass
+
+class Krepla(BoxLayout):
+    line_count = 0
+    io_history = ObjectProperty(None)
+    def run_input(self, command):
+        self.line_count += 1
+        row = HistoryInput()
+        row.line_num = str(self.line_count)
+        row.input_text = command
+        self.io_history.add_widget(row)
 
 
 class KreplaApp(App):
